64907a8709ab2efb943074dcd0c979451cedad4d
lem0nado <82136329+lem0nado@users.noreply.github.com>
2021-12-09 17:35:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,29 +10,7 @@ import smtplib
 from os import environ
 from email.mime.text import MIMEText
 
-# EMAIL_USERNAME = environ['EMAIL_USERNAME']
-# EMAIL_TO = environ['EMAIL_TO']
-# EMAIL_FROM = environ['EMAIL_FROM']
-# EMAIL_PASSWORD = environ['EMAIL_PASSWORD']
-# EMAIL_SERVER = environ['EMAIL_SERVER']
-# EMAIL_PORT = int(environ['EMAIL_PORT'])
-core = {
-"id": "2c95de297d8646d5017d926443b95246",
-"token": "404978DE4DFC3B4736B32B1D2A58D65D",
-"c16": "在校且住宿",
-"c17": "在京",
-"c18": "低风险地区",
-"c15": "无情况",
-"type": "YQSJSB",
-"location_longitude": 116.480287,
-"location_latitude": 39.873798,
-"location_address": "北京市朝阳区南磨房乡北京工业大学"
-}
-
-# json.loads(environ['DATA'])
-
-
-result = ""
+core = json.loads(environ['DATA'])
 
 URL_SESSION = 'http://xgxt.bjut.edu.cn/nonlogin/qywx/authentication.htm?appId=2c95de297d4f8bfa017d8631748b7fe2&urlb64=L3dlYkFwcC94dWVnb25nL2luZGV4Lmh0bWwjL2FjdGlvbi9iYXNlSW5kZXgvJUU3JUE3JUJCJUU1JThBJUE4JUU1JUFEJUE2JUU1JUI3JUE1'
 URL_CLOCKIN = 'http://xgxt.bjut.edu.cn/syt/zzapply/operation.htm'
@@ -43,22 +21,16 @@ HEADER_SESSION = {
     'Accept-Encoding': 'gzip, deflate, br',
     'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-US;q=0.7',
     'Connection': 'keep-alive',
-    'X-Requested-With': 'com.tencent.wework',
-    'User-Agent': 'Mozilla/5.0 (Linux; Android 11; GM1910 Build/RKQ1.201022.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/95.0.4638.74 Mobile Safari/537.36 wxwork/3.1.7 MicroMessenger/7.0.1 NetType/4G Language/zh Lang/zh'
+    'Host': 'bjut.sanyth.com:81',
+    'Cookie': 'id='+core['id']+'; token='+core['token'],
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/85.0.4183.83 Safari/537.36 '
 }
-
 s = requests.Session()
-s.cookies.set('id', core['id'])
-s.cookies.set('token', core['token'])
-
 response = s.get(URL_SESSION, headers=HEADER_SESSION)
-print(response)
-print(response.text)
-print(response.headers)
-if response.status_code != 200:
-    result += "打卡失败！错误：获取session失败" + str(response.status_code)
-    result += response.text
-    
+session = response.history[0].headers['Set-Cookie'].split(';')[0]
+cookie = 'id=' + core['id'] + '; token=' + core['token'] + '; ' + session
+
 # Part2 Get header & data
 HEADER = {
     'Accept': 'application/json, text/plain, */*',
@@ -66,8 +38,11 @@ HEADER = {
     'Accept-Language': 'zh-CN,zh-Hans;q=0.9',
     'Connection': 'keep-alive',
     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    'Origin': 'http://xgxt.bjut.edu.cn',
-    'Referer': 'http://xgxt.bjut.edu.cn/webApp/xuegong/index.html',
+    'Content-length': '1150',
+    'Cookie': cookie,
+    'Host': 'bjut.sanyth.com:81',
+    'Origin': 'http://bjut.sanyth.com:81',
+    'Referer': 'http://bjut.sanyth.com:81/webApp/xuegong/index.html',
     'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko)  Mobile/15E148 wxwork/3.1.18 MicroMessenger/7.0.1 Language/zh ColorScheme/Dark'
     'Chrome/85.0.4183.83 Safari/537.36',
     'X-Requested-With': 'XMLHttpRequest',
@@ -76,8 +51,9 @@ HEADER = {
 # user info
 info = {
     'xmqkb': {
-        'id': '2c95de297d4f8bfa017d85f53d267613'
+        'id': '402880c97b1c114b017b1c2af13d02d8'
     },
+    'c12': core['c12'],
     'c15': core['c15'],
     'c16': core['c16'],
     'c17': core['c17'],
@@ -96,17 +72,15 @@ prefix_raw = 'data='+urllib.parse.quote_plus(prefix_data)
 DATA = prefix_raw + suffix_raw
 
 # Part3 Clock in
-response_clockin = s.post(url=URL_CLOCKIN, headers=HEADER, data=DATA)
-print(response_clockin)
-print(response_clockin.text)
-print(response_clockin.headers)
-# result = '打卡失败'
+response_clockin = requests.post(url=URL_CLOCKIN, headers=HEADER, data=DATA)
+
+result = '打卡失败'
 
 if response_clockin.text == 'success':
-    result += '打卡成功'
+    result = '打卡成功'
 else:
     if response_clockin.text == 'Applied today':
-        result += '今天已经打过卡'
+        result = '今天已经打过卡'
     else:
         result += f'''
 HTTP status: {response_clockin.status_code}
@@ -115,15 +89,5 @@ HTTP status: {response_clockin.status_code}
     json.dumps(info, ensure_ascii=False, sort_keys=True, indent=2)
 }
 '''
-print(result)
-message = MIMEText(result, 'plain', 'utf-8')
-message['Subject'] = '打卡结果'
-message['FROM'] = EMAIL_FROM
-message['To'] = EMAIL_TO
 
-server = smtplib.SMTP(EMAIL_SERVER)
-server.connect(EMAIL_SERVER, EMAIL_PORT)
-server.ehlo()
-server.starttls()
-server.login(EMAIL_USERNAME, EMAIL_PASSWORD)
-server.sendmail(EMAIL_USERNAME, [EMAIL_USERNAME], message.as_string())
+print(result)
